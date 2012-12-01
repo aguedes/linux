09b5470ad2d779c2074cd734365b5b0f32886ef4
Andre Guedes <andre.guedes@openbossa.org>
2012-11-30 21:12:57 -0300

    WIP: Bluetooth: Use LED generic handler
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -157,9 +157,11 @@ static int hidp_queue_event(struct hidp_session *session, struct input_dev *dev,
 static int hidp_hidinput_event(struct input_dev *dev, unsigned int type, unsigned int code, int value)
 {
 	struct hid_device *hid = input_get_drvdata(dev);
-	struct hidp_session *session = hid->driver_data;
 
-	return hidp_queue_event(session, dev, type, code, value);
+	if (type != EV_LED)
+		return -1;
+
+	return hidinput_led_output_report(hid, code, value);
 }
 
 static int hidp_input_event(struct input_dev *dev, unsigned int type, unsigned int code, int value)
