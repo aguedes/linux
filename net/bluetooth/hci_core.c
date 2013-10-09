33867e4b160676353b92412b6f76d4590f8878f7
Andre Guedes <andre.guedes@openbossa.org>
2013-10-09 14:46:51 -0300

    Bluetooth: Introduce connection parameters list
    This patch adds to hdev the connection parameters list (hdev->
    conn_param). The elements from this list (struct hci_conn_param)
    contains the connection parameters (for now, minimum and maximum
    connection interval) that should be used for establishing
    connection.
    
    The struct hci_conn_param also defines the 'auto_connect' field
    which will be used to implement the auto connection mechanism.
    
    Moreover, this patch adds helper functions to manipulate hdev->conn_
    param list. Some of these functions are also declared in hci_core.h
    since they will be used outside hci_core.c in upcoming patches.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -2189,6 +2189,136 @@ int hci_blacklist_del(struct hci_dev *hdev, bdaddr_t *bdaddr, u8 type)
 	return mgmt_device_unblocked(hdev, bdaddr, type);
 }
 
+struct hci_conn_param *find_conn_param(struct hci_dev *hdev,
+				       bdaddr_t *addr, u8 addr_type)
+{
+	struct hci_conn_param *param;
+
+	rcu_read_lock();
+
+	list_for_each_entry(param, &hdev->conn_param, list) {
+		if (bacmp(&param->addr, addr))
+			continue;
+		if (param->addr_type != addr_type)
+			continue;
+
+		rcu_read_unlock();
+		return param;
+	}
+
+	rcu_read_unlock();
+	return NULL;
+}
+
+static struct hci_conn_param *new_conn_param(bdaddr_t *addr, u8 addr_type,
+					     u8 auto_connect,
+					     u16 min_conn_interval,
+					     u16 max_conn_interval)
+{
+	struct hci_conn_param *param;
+
+	param = kmalloc(sizeof(*param), GFP_KERNEL);
+	if (!param)
+		return NULL;
+
+	bacpy(&param->addr, addr);
+	param->addr_type = addr_type;
+	param->auto_connect = auto_connect;
+	param->min_conn_interval = min_conn_interval;
+	param->max_conn_interval = max_conn_interval;
+	return param;
+}
+
+static int update_conn_param(struct hci_dev *hdev, struct hci_conn_param *old,
+			     u8 auto_connect, u16 min_conn_interval,
+			     u16 max_conn_interval)
+{
+	struct hci_conn_param *new;
+
+	new = new_conn_param(&old->addr, old->addr_type, auto_connect,
+			     min_conn_interval, max_conn_interval);
+	if (!new)
+		return -ENOMEM;
+
+	hci_dev_lock(hdev);
+	list_replace_rcu(&old->list, &new->list);
+	hci_dev_unlock(hdev);
+
+	synchronize_rcu();
+
+	kfree(old);
+	return 0;
+}
+
+/*
+ * Add a new connection parameter object to hdev->conn_param list.
+ *
+ * If the hci_conn_param object already exists, we update the auto_connect,
+ * min_conn_interval and max_conn_interval values.
+ *
+ * Return 0 if connection parameter was added successfully.
+ */
+int hci_add_conn_param(struct hci_dev *hdev, bdaddr_t *addr, u8 addr_type,
+		       u8 auto_connect, u16 min_conn_interval,
+		       u16 max_conn_interval)
+{
+	struct hci_conn_param *param;
+
+	param = find_conn_param(hdev, addr, addr_type);
+	if (param)
+		return update_conn_param(hdev, param, auto_connect,
+					 min_conn_interval, max_conn_interval);
+
+	param = new_conn_param(addr, addr_type, auto_connect,
+			       min_conn_interval, max_conn_interval);
+	if (!param)
+		return -ENOMEM;
+
+	hci_dev_lock(hdev);
+	list_add_rcu(&param->list, &hdev->conn_param);
+	hci_dev_unlock(hdev);
+	return 0;
+}
+
+/*
+ * Remove from hdev->conn_param and free hci_conn_param.
+ *
+ * This function requires the caller holds hdev->lock.
+ */
+static void __remove_conn_param(struct hci_conn_param *param)
+{
+	list_del_rcu(&param->list);
+	synchronize_rcu();
+
+	kfree(param);
+}
+
+void hci_remove_conn_param(struct hci_dev *hdev, bdaddr_t *addr, u8 addr_type)
+{
+	struct hci_conn_param *param;
+
+	param = find_conn_param(hdev, addr, addr_type);
+	if (!param)
+		return;
+
+	hci_dev_lock(hdev);
+	__remove_conn_param(param);
+	hci_dev_unlock(hdev);
+}
+
+/*
+ * Remove all elements from hdev->conn_param list.
+ *
+ * This function requires the caller holds hdev->lock.
+ */
+static void __clear_conn_param(struct hci_dev *hdev)
+{
+	struct hci_conn_param *param, *tmp;
+
+	list_for_each_entry_safe(param, tmp, &hdev->conn_param, list)
+		__remove_conn_param(param);
+}
+
 static void inquiry_complete(struct hci_dev *hdev, u8 status)
 {
 	if (status) {
@@ -2297,6 +2427,7 @@ struct hci_dev *hci_alloc_dev(void)
 	INIT_LIST_HEAD(&hdev->link_keys);
 	INIT_LIST_HEAD(&hdev->long_term_keys);
 	INIT_LIST_HEAD(&hdev->remote_oob_data);
+	INIT_LIST_HEAD(&hdev->conn_param);
 	INIT_LIST_HEAD(&hdev->conn_hash.list);
 
 	INIT_WORK(&hdev->rx_work, hci_rx_work);
@@ -2475,6 +2606,7 @@ void hci_unregister_dev(struct hci_dev *hdev)
 	hci_link_keys_clear(hdev);
 	hci_smp_ltks_clear(hdev);
 	hci_remote_oob_data_clear(hdev);
+	__clear_conn_param(hdev);
 	hci_dev_unlock(hdev);
 
 	hci_dev_put(hdev);
