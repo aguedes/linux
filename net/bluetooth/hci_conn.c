72c6f7f539278202ea34706d54811fb65be9c04b
Andre Guedes <andre.guedes@openbossa.org>
2014-04-15 18:31:27 -0300

    Bluetooth: Export slave latency parameter
    This patch exports slave latency parameter through Bluetooth debugfs
    interface. The default value is 0 and it may be changed by writing the
    new value in conn_latenty file. The acceptable values range from 0x0000
    to 0x01F3, according described in Core 4.1, Vol 2, 7.8.12.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -622,6 +622,7 @@ static void hci_req_add_le_create_conn(struct hci_request *req,
 	cp.conn_interval_min = cpu_to_le16(conn->le_conn_min_interval);
 	cp.conn_interval_max = cpu_to_le16(conn->le_conn_max_interval);
 	cp.supervision_timeout = cpu_to_le16(0x002a);
+	cp.conn_latency = hdev->le_conn_latency;
 	cp.min_ce_len = cpu_to_le16(0x0000);
 	cp.max_ce_len = cpu_to_le16(0x0000);
 
