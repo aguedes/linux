ce0410add81fb61332b36c6a7780a4756ae97aa5
Andre Guedes <andre.guedes@openbossa.org>
2014-04-15 18:50:02 -0300

    Bluetooth: Export supervision timeout parameter
    This patch exports supervision timeout parameter through Bluetooth
    debugfs interface. The default value is 0x002a and it may be changed by
    writing the new value in conn_supervision_timeout file. The acceptable
    values range from 0x000a to 0x0c80 according described in Core 4.1,
    Vol 2, 7.8.12.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>

--- net/bluetooth/hci_conn.c
+++ net/bluetooth/hci_conn.c
@@ -621,7 +621,7 @@ static void hci_req_add_le_create_conn(struct hci_request *req,
 	cp.own_address_type = own_addr_type;
 	cp.conn_interval_min = cpu_to_le16(conn->le_conn_min_interval);
 	cp.conn_interval_max = cpu_to_le16(conn->le_conn_max_interval);
-	cp.supervision_timeout = cpu_to_le16(0x002a);
+	cp.supervision_timeout = cpu_to_le16(hdev->le_conn_supervision_timeo);
 	cp.conn_latency = hdev->le_conn_latency;
 	cp.min_ce_len = cpu_to_le16(0x0000);
 	cp.max_ce_len = cpu_to_le16(0x0000);
