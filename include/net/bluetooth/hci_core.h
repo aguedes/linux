0fd300fe7253591212ddefa56dd37149b1d94d0e
Andre Guedes <andre.guedes@openbossa.org>
2014-05-21 15:11:57 -0300

    Bluetooth: Extend LE connection parameters infrastructure
    This patch extends connection parameters infrastructure (hci_conn_
    params and helper functions) to consider slave latency and supervision
    timeout parameters.
    
    Signed-off-by: Andre Guedes <andre.guedes@openbossa.org>

--- include/net/bluetooth/hci_core.h
+++ include/net/bluetooth/hci_core.h
@@ -437,6 +437,8 @@ struct hci_conn_params {
 
 	u16 conn_min_interval;
 	u16 conn_max_interval;
+	u16 latency;
+	u16 supervision_timeo;
 
 	enum {
 		HCI_AUTO_CONN_DISABLED,
@@ -843,7 +845,8 @@ struct hci_conn_params *hci_conn_params_lookup(struct hci_dev *hdev,
 					       bdaddr_t *addr, u8 addr_type);
 int hci_conn_params_add(struct hci_dev *hdev, bdaddr_t *addr, u8 addr_type,
 			u8 auto_connect, u16 conn_min_interval,
-			u16 conn_max_interval);
+			u16 conn_max_interval, u16 latency,
+			u16 supervision_timeo);
 void hci_conn_params_del(struct hci_dev *hdev, bdaddr_t *addr, u8 addr_type);
 void hci_conn_params_clear(struct hci_dev *hdev);
 
