a3a42fc602ca895e1f70dee3f2c4800decbd87f2
Andre Guedes <andre.guedes@intel.com>
2019-12-11 18:26:58 -0800

    ether: Add ETH_P_AVTP macro
    This patch adds the ETH_P_AVTP macro which defines the Audio/Video
    Transport Protocol (AVTP) ethertype assigned to 0x22F0, according to:
    
    http://standards-oui.ieee.org/ethertype/eth.txt
    
    AVTP is the transport protocol utilized in Audio/Video Bridging (AVB),
    and it is defined by IEEE 1722 standard.
    
    Note that we have ETH_P_TSN macro defined with the number assigned to
    AVTP. However, there is no "TSN" ethertype. TSN is not a protocol, but a
    set of features to deliver networking determinism, so ETH_P_TSN can be a
    bit misleading. For compatibility reasons we should keep it around.
    This patch re-defines it using the ETH_P_AVTP macro to make it explicit.
    
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>

--- include/uapi/linux/if_ether.h
+++ include/uapi/linux/if_ether.h
@@ -47,7 +47,8 @@
 #define ETH_P_LOOP	0x0060		/* Ethernet Loopback packet	*/
 #define ETH_P_PUP	0x0200		/* Xerox PUP packet		*/
 #define ETH_P_PUPAT	0x0201		/* Xerox PUP Addr Trans packet	*/
-#define ETH_P_TSN	0x22F0		/* TSN (IEEE 1722) packet	*/
+#define ETH_P_AVTP	0x22F0		/* AVTP (IEEE 1722)		*/
+#define ETH_P_TSN	ETH_P_AVTP	/* There is no TSN ethertype, we define it for compatibility reasons */
 #define ETH_P_ERSPAN2	0x22EB		/* ERSPAN version 2 (type III)	*/
 #define ETH_P_IP	0x0800		/* Internet Protocol packet	*/
 #define ETH_P_X25	0x0805		/* CCITT X.25			*/
