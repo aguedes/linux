d203ceec487f466a36c9a9d90c3b43e6d2e832a9
Jesper Dangaard Brouer <brouer@redhat.com>
2020-04-30 12:54:50 -0700

    ixgbe: fix XDP redirect on archs with PAGE_SIZE above 4K
    The ixgbe driver have another memory model when compiled on archs with
    PAGE_SIZE above 4096 bytes. In this mode it doesn't split the page in
    two halves, but instead increment rx_buffer->page_offset by truesize of
    packet (which include headroom and tailroom for skb_shared_info).
    
    This is done correctly in ixgbe_build_skb(), but in ixgbe_rx_buffer_flip
    which is currently only called on XDP_TX and XDP_REDIRECT, it forgets
    to add the tailroom for skb_shared_info. This breaks XDP_REDIRECT, for
    veth and cpumap.  Fix by adding size of skb_shared_info tailroom.
    
    Fixes: 6453073987ba ("ixgbe: add initial support for xdp redirect")
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -2266,10 +2266,16 @@ static void ixgbe_rx_buffer_flip(struct ixgbe_ring *rx_ring,
 				 struct ixgbe_rx_buffer *rx_buffer,
 				 unsigned int size)
 {
-	unsigned int truesize = ixgbe_rx_frame_truesize(rx_ring, size);
 #if (PAGE_SIZE < 8192)
+	unsigned int truesize = ixgbe_rx_frame_truesize(rx_ring, size);
+
 	rx_buffer->page_offset ^= truesize;
 #else
+	unsigned int truesize = ring_uses_build_skb(rx_ring) ?
+				SKB_DATA_ALIGN(IXGBE_SKB_PAD + size) +
+				SKB_DATA_ALIGN(sizeof(struct skb_shared_info)) :
+				SKB_DATA_ALIGN(size);
+
 	rx_buffer->page_offset += truesize;
 #endif
 }
