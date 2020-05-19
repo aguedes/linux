8c9c667244d0dd2166731e2a393323101d9b79b3
Alan Brady <alan.brady@intel.com>
2020-05-18 22:13:26 -0700

    idpf: Introduce idpf driver
    Utilizes the Intel Ethernet Common Module and provides
    a device specific implementation for data plane devices.
    
    Signed-off-by: Alan Brady <Alan.Brady@intel.com>
    Signed-off-by: Alice Michael <alice.michael@intel.com>
    Signed-off-by: Phani Burra <phani.r.burra@intel.com>
    Signed-off-by: Joshua Hay <joshua.a.hay@intel.com>
    Signed-off-by: Madhu Chittim <madhu.chittim@intel.com>
    Signed-off-by: Pavan Kumar Linga <Pavan.Kumar.Linga@intel.com>
    Reviewed-by: Donald Skidmore <donald.c.skidmore@intel.com>
    Reviewed-by: Jesse Brandeburg <jesse.brandeburg@intel.com>
    Reviewed-by: Sridhar Samudrala <sridhar.samudrala@intel.com>
    Reported-by: kbuild test robot <lkp@intel.com>

---
+++ drivers/net/ethernet/intel/idpf/idpf_devids.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright (C) 2020 Intel Corporation */
+
+#ifndef _IDPF_DEVIDS_H_
+#define _IDPF_DEVIDS_H_
+
+/* Device IDs */
+#define IDPF_DEV_ID_PF			0x1452
+
+#endif /* _IDPF_DEVIDS_H_ */
