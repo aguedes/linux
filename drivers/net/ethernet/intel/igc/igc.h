4670c3585d0e7abf85a69e1d885bef74b28d98ce
Andre Guedes <andre.guedes@intel.com>
2020-08-20 17:24:20 -0700

    igc: Add Initial XDP support
    This patch adds the initial XDP support to the igc driver. For now,
    only XDP_PASS, XDP_DROP, XDP_ABORT actions are supported. Upcoming
    patches will add support for the remaining XDP actions.
    
    XDP-specific helpers are defined in a new file, igc_xdp.c. These
    helpers are utilized in igc_main.c to implement the ndo_bpf callback
    and to handle XDP programs execution in igc_clean_rx_irq(). The driver
    doesn't support XDP functionality with frames that span over multiple
    buffers so jumbo frames are not allowed for now.
    
    The approach implemented by this patch follows the approach implemented
    in other Intel drivers as much as possible for the sake of consistency
    across the drivers.
    
    Quick comment regarding igc_build_skb(): this patch doesn't touch it
    because the function is never called. It seems its support is
    incomplete/in progress. The function was added by commit 0507ef8a0372b
    ("igc: Add transmit and receive fastpath and interrupt handlers") but
    ring_ uses_build_skb() always return False since the IGC_RING_FLAG_RX_
    BUILD_SKB_ENABLED isn't set anywhere in the driver code.
    
    This patch has been tested with the sample app "xdp1" located in
    samples/bpf/ dir.
    
    Signed-off-by: Andre Guedes <andre.guedes@intel.com>

--- drivers/net/ethernet/intel/igc/igc.h
+++ drivers/net/ethernet/intel/igc/igc.h
@@ -217,6 +217,8 @@ struct igc_adapter {
 	struct timecounter tc;
 	struct timespec64 prev_ptp_time; /* Pre-reset PTP clock */
 	ktime_t ptp_reset_start; /* Reset time in clock mono */
+
+	struct bpf_prog *xdp_prog;
 };
 
 void igc_up(struct igc_adapter *adapter);
