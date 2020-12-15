22794c56704f5d791d9c2e5a5f8c3971f575e1fb
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2020-12-14 16:09:47 -0800

    i40e: drop redundant check when setting xdp prog
    Net core handles the case where netdev has no xdp prog attached and
    current prog is NULL. Therefore, remove such check within
    i40e_xdp_setup.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -12947,9 +12947,6 @@ static int i40e_xdp_setup(struct i40e_vsi *vsi,
 	if (frame_size > vsi->rx_buf_len)
 		return -EINVAL;
 
-	if (!i40e_enabled_xdp_vsi(vsi) && !prog)
-		return 0;
-
 	/* When turning XDP on->off/off->on we reset and rebuild the rings. */
 	need_reset = (i40e_enabled_xdp_vsi(vsi) != !!prog);
 
